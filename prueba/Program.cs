65d5149e2002dfa14d328da00d3e4a561e9f347e
Demián Diaz <beencouch@gmail.com>
2026-03-25 22:03:00 -0300

    hola mundo

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,11 @@ namespace prueba
     {
         static void Main(string[] args)
         {
+            {
+                string mensaje = "hola mundo";
+            }
+
         }
     }
 }
+
